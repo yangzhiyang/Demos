adb061c7d79093ae8ae99acf38842cb9ed6de2b3
冬霖 <zhiyang_yang@126.com>
2020-02-04 20:03:35 +0800

    feat: 增加demo

---
+++ src/pages/index.js
@@ -0,0 +1,57 @@
+import React from "react";
+
+import { Select } from "antd";
+import SelectPlus from "../component/SelectPlus";
+import "antd/dist/antd.css";
+
+const Option = Select.Option;
+
+const children = [];
+
+for (let i = 0; i < 10000; i++) {
+  children.push(
+    <Option value={i + "xx"} key={i}>
+      {i}
+    </Option>
+  );
+}
+
+class App extends React.Component {
+  onChange = v => console.log(v);
+  onSearch = v => console.log(v);
+
+  render() {
+    return (
+      <div className="App">
+        <div style={{ width: 300 }}>
+          SelectPlus:
+          <SelectPlus
+            showSearch
+            allowClear
+            // mode="multiple"
+            onChange={this.onChange}
+            onSearch={this.onSearch}
+            style={{ width: 300 }}
+          >
+            {children}
+          </SelectPlus>
+        </div>
+        <div style={{ width: 300 }}>
+          Ant Select:
+          <Select
+            showSearch
+            allowClear
+            // mode="multiple"
+            onChange={this.onChange}
+            onSearch={this.onSearch}
+            style={{ width: 300 }}
+          >
+            {children}
+          </Select>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
